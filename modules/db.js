d39cbdb4d65b9dffb8c049a3c60ca41be951da12
Christoph Herrmann <developer@dragonprojects.de>
2015-02-12 21:21:04 +0100

    Refactoring of the collection binding, use configuration instead of service

---
+++ modules/db.js
@@ -0,0 +1,20 @@
+"use strict";
+/*global module:false */
+
+/**
+ * Initialize db service with the MongoDB connection and the toObjectID converter
+ * @example
+    db: {
+        uri: 'mongodb://127.0.0.1/app?auto_reconnect=true',
+        options: { safe: true }
+    }
+ */
+
+module.exports = function (config, libraries, services) {
+    var mongoskin = libraries.mongoskin;
+
+    var db = mongoskin.db(config.uri, config.options);
+    db.toObjectID = mongoskin.helper.toObjectID;
+
+    services.db = db;
+};
